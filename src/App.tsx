6784e0c0647a37fb7ea788be6da061b09b086185
Chiranjeev Vyas <75154547+thechiranjeevvyas@users.noreply.github.com>
2024-09-29 03:23:04 +0530

    children class and alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import ListGroup from "./components/ListGroup";
+// //import ListGroup from "./components/ListGroup";
 
+// function App() {
+//   let items = ["Delhi", "Mumbai", "Kolkata", "Banglore", "Chennai"];
+
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />{" "}
+//     </div>
+//   );
+// }
+// export default App;
+import Alert from "./components/Alert";
 function App() {
-  let items = ["Delhi", "Mumbai", "Kolkata", "Banglore", "Chennai"];
-
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />{" "}
+      <Alert>
+        Hehe i <span>have learnt it</span>
+      </Alert>
     </div>
   );
 }
